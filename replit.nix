e1ad4bd2eeccf7953b2dbabb4270614eb9ce1be7
KoPlayz <koplayzmail@pm.me>
2024-05-23 20:24:13 -0400

    Uploaded KSFXB code

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+  deps = [
+    pkgs.
+    pkgs.sysvinit
+    pkgs.zsh
+    pkgs.nano
+    pkgs.neofetch
+    pkgs.wget
+pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
+  ];
+}
---
+++ script.js
@@ -0,0 +1,63 @@
+function playwah() {
+  var audio = document.getElementById("wahwahaudio");
+  audio.play();
+}
+
+function playsus() {
+  var audio =
+    document.getElementById("susaudio");
+  audio.play();
+}
+function playmeow() {
+  var audio =
+    document.getElementById("meowaudio");
+  audio.play();
+}
+
+function playsad() {
+  var audio =
+    document.getElementById("sadaudio");
+  audio.play();
+}
+
+function playquack() {
+  var audio =
+    document.getElementById("quackaudio");
+  audio.play();
+}
+
+function playjeo() {
+  var audio =
+    document.getElementById("jeoaudio");
+  audio.play();
+}
+
+function playspac() {
+  var audio =
+    document.getElementById("spacaudio");
+  audio.play();
+}
+
+function playvint() {
+  var audio =
+    document.getElementById("vintaudio");
+  audio.play();
+}
+
+function playdisc() {
+  var audio =
+    document.getElementById("discaudio");
+  audio.play();
+}
+
+function playbruh() {
+  var audio =
+    document.getElementById("bruhaudio");
+  audio.play();
+}
+
+function playhorn() {
+  var audio =
+    document.getElementById("airhornaudio");
+  audio.play();
+}
